a78c72eef8df46c7a15e70ae48263f7c3f14c67a
Elfikk <Jaroslaw.Wiktor.Ciba@gmail.com>
2025-10-04 17:48:56 +0100

    Add Dumping to CSV

---
+++ utils/sprinkle_dump.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "event.hpp"
+
+#include "sprinkler/sprinkle.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+namespace CausalUtils
+{
+
+// Dump Event into a CSV Format
+template<int d>
+void dumpEventCsv(std::ofstream & stream, const Event<d> & event)
+{
+    for (unsigned int i = 0; i < d; i++)
+    {
+        auto component = event[i];
+        stream << component << ",";
+    }
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+
+// Function For Dumping a Sprinkle to a CSV
+template<int d>
+void dumpSprinkleCsv(Sprinkle<d> & sprinkle, const std::string filename)
+{
+    auto events = sprinkle.getEvents();
+    std::ofstream dumpFile(filename);
+    for (const auto & event : events)
+    {
+        dumpEventCsv(dumpFile, event);
+        dumpFile << '\n';
+    }
+    dumpFile.close();
+}
+
+
+}
+
