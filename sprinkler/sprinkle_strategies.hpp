3f954b6f7e60a88fbd163e2a612dd3dbc34e1ed6
Elfikk <Jaroslaw.Wiktor.Ciba@gmail.com>
2025-10-05 18:01:42 +0100

    Fix up AdS Region Sprinkling

--- sprinkler/sprinkle_strategies.hpp
+++ sprinkler/sprinkle_strategies.hpp
@@ -55,21 +55,30 @@ namespace SprinkleStrategy
 //---------------------------------------------------------------------------------------------------------------------
 
     template<int d>
-    std::optional<Event<d>> adsRegionSprinkleEvent(Region<d> & sprinkleRegion, RectangularRegion<d> & enclosingRegion)
+    std::optional<Event<d>> adsRegionSprinkleEvent(Region<d> & sprinkleRegion, RectangularRegion<d> & enclosingRegion, Spacetime<d> & spacetime)
     {
         const auto randomNums = SprinkleUtils::generateRandomNumbers<d>();
         // Now map random numbers to coordinates within enclosing region
         std::array<double, d> coords;
+        const auto R1 = spacetime.getUpperBound(1);
+        const auto R0 = spacetime.getLowerBound(1);
+        const auto R0_pow = pow(R0, 1 - d);
+        const auto R1_pow = pow(R1, 1 - d);
         for (int i = 0; i < d; i++)
         {
-            coords[i] = SprinkleUtils::linearInterpolate(
-                randomNums[i],
-                enclosingRegion.getLowerBound(i),
-                enclosingRegion.getUpperBound(i)
-            );
+            if (i != 1)
+            {
+                coords[i] = SprinkleUtils::linearInterpolate(
+                    randomNums[i],
+                    enclosingRegion.getLowerBound(i),
+                    enclosingRegion.getUpperBound(i)
+                );
+            }
+            else
+            {
+                coords[i] = pow(R0_pow - randomNums[i] * (R0_pow - R1_pow), 1 / (1 - d));
+            }
         }
-        const auto R0 = sprinkleRegion.getR0();
-        coords[1] = R0 / pow(1 - randomNums[1], 1 / d-1);
 
         Event<d> event(coords);
         return sprinkleRegion.isInside(event) ? std::optional(event) : std::nullopt;
