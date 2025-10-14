0c421c3bdd1a0c12354532d4ef884e9b5e36b2a7
Elfikk <Jaroslaw.Wiktor.Ciba@gmail.com>
2025-10-14 19:50:41 +0100

    Terrible Main Commit

--- main.cpp
+++ main.cpp
@@ -1,57 +1,75 @@
-// #include "graphs/DirectedGraph.hpp"
-
 #include "event.hpp"
 
+#include "spacetimes/de_sitter.hpp"
 #include "spacetimes/minkowski.hpp"
 
 #include "sprinkler/sprinkler_builder.hpp"
 
 #include "utils/causal_functions.hpp"
+#include "utils/graph_dump.hpp"
+#include "utils/sprinkle_dump.hpp"
 
 #include <iostream>
 
 int main(int argc, char * argv[])
 {
-    // auto graph = DirectedGraph(7);
-    // graph.removeNode(0);
-
-    // // 1 -> 4, 5
-    // graph.addEdge(1, 4);
-    // graph.addEdge(1, 5);
-
-    // // 2 -> 4, 6
-    // graph.addEdge(2, 4);
-    // graph.addEdge(2, 6);
-
-    // // 3 -> 5, 6
-    // graph.addEdge(3, 5);
-    // graph.addEdge(3, 6);
+    const auto PI = 3.141592653589793238462643383279502884L;
 
     // Wow...
+    const auto d = 2;
+    auto builder = SprinklerBuilder<d>();
+    const auto R0 = 0.01;
+    // auto spacetime = builder.buildAds(R0);
+    // auto spacetime = builder.buildMinkowski();
+    auto spacetime = builder.buildDeSitter({-0.45 * PI, 0.45 * PI, R0, 0.45 * PI + R0});
 
-    auto builder = SprinklerBuilder<2>();
-    builder.buildAds(0.01);
+    std::cout << "Made Spacetime\n";
 
-    auto bottom = Event<2>({-1, 0});
-    auto top = Event<2>({1, 0});
+    auto bottom = Event<d>({-0.45 * PI, R0});
+    auto top = Event<d>({0.45 * PI, R0});
 
-    builder.buildCausalRegion(bottom, top);
+    // std::array<long double, 2> extension({R0, 2 + R0});
 
-    std::array<double, 4> bounds({-1, 1, -1, 1});
+    auto region = builder.buildCausalRegion(bottom, top);
+    // auto region = builder.buildExtendedCausalRegion(bottom, top, extension, 1);
 
-    builder.buildRectangularEnclosure(bounds);
+    // auto region = builder.buildRectangularRegion({-0.45 * PI, 0.45 * PI, - PI, PI});
 
-    auto sprinkler = builder.getSprinkler();
+    // auto r = 1.0;
+    // auto L = 1.0;
+    // auto cylinderCentre = Event<d>({0, R0 + r, 0});
+    // auto sphereCentre = Event<d>({0, 0, 0});
+
+    // auto region = builder.buildCylinderRegion(0, cylinderCentre, L, r);
+    // auto region = builder.buildSphericalRegion(sphereCentre, r);
+
+    std::cout << "Made Region\n";
+
+    std::array<long double, 2 * d> bounds({-0.45 * PI, 0.45 * PI, R0, 0.45 * PI + R0});
+    // std::array<long double, 2 * d> bounds({-L, L, R0, R0 + 2 * r, -r, r});
+    // std::array<long double, 2 * d> bounds({-r, r, R0, R0 + 2*r, -r, r});
+    // std::array<long double, 2*d> bounds({-r, r, -r, r, -r, r});
+
+    auto enclosure = builder.buildRectangularEnclosure(bounds);
+    std::cout << "Made Enclosure\n";
+
+    auto sprinkler = builder.getSprinkler(spacetime, region, enclosure);
+    // std::cout << "Got Sprinkler\n";
 
     if (sprinkler.canSprinkle())
     {
-        auto sprinkle = sprinkler.sprinkle(10);
-        auto graph = sprinkle.generateCausalSet(sprinkler.getSpacetime());
+        std::cout << "Values good!\n";
+        auto sprinkle = sprinkler.sprinkle(1000);
+        std::cout << "Sprinkled.";
+        CausalUtils::dumpSprinkleCsv(sprinkle, "/mnt/c/Projekty/Coding/C++/Sprinkling/data/refactor/test_ds_half_2_1000.csv");
 
-        std::cout << "\nAnything?";
+        auto graph = sprinkle.generateCausalSet();
+
+        GraphUtils::dumpDagToCsv(graph, "/mnt/c/Projekty/Coding/C++/Sprinkling/data/refactor/dag_ds_half_2_1000.csv");
+
+        auto longestPath = graph.findLongestPath();
+        GraphUtils::dumpDagLongestPathToCsv(longestPath, "/mnt/c/Projekty/Coding/C++/Sprinkling/data/refactor/path_ds_half_2_1000.csv");
     }
-
-    std::cout << "\nNo?";
 
     return 0;
 }
